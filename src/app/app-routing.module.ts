8e8b0db8904f199a0ffe5e407649b1646d608616
rahimrahu <mohammad.rahimuddin786@gmail.com>
2019-08-16 12:25:10 +0530

    routing is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-
+import { SignupComponent} from './signup/signup.component';
 const routes: Routes = [
   {
     path:'',
     component : HomeComponent
   },
-  { path:'login', component : LoginComponent }
+  { path:'login', component : LoginComponent
+},
+{ path:'signup',component : SignupComponent}
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
   ],
-  declarations: []
+  declarations: [],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
